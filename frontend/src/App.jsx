2b2241399c0d9364ae8cc62005f9a07965eb260b
Jean Reis <jeanreis.developer@gmail.com>
2026-08-31 16:36:33 -0300

    feat: replace React frontend with Angular

--- frontend/src/App.jsx
+++
@@ -1,6 +0,0 @@
-import { LoginPage } from '@/features/auth/LoginPage'
-import { SignUpPage } from '@/features/auth/SignUpPage'
-
-export default function App() {
-  return window.location.pathname === '/signup' ? <SignUpPage /> : <LoginPage />
-}
